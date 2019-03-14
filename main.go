38095b013d8b8241d29783f312a910b58714ada4
nian.zhou <nian.zhou@office.feng.com>
2019-03-14 22:02:14 +0800

    save

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"GTMS/conf"
 	_ "GTMS/routers"
 	"github.com/astaxie/beego"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	conf.LoadingConf()
 	boot.ConnectMySQL()
 	boot.ConnectRedis()
