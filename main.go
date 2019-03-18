1260f663752d887da57913379b4572fee3ed3bc4
nian.zhou <nian.zhou@office.feng.com>
2019-03-18 14:57:20 +0800

    add log

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"GTMS/conf"
 	_ "GTMS/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"runtime"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	conf.LoadingConf()
+	logs.SetLogger(logs.AdapterMultiFile, `{"filename":"./runtime/log/project.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
+	logs.Async()
 	boot.ConnectMySQL()
 	boot.ConnectRedis()
 	beego.Run()
