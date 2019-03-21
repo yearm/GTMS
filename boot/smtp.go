84be60849e7a732f08a2f49b9d0c4448773e3fa2
nian.zhou <nian.zhou@office.feng.com>
2019-03-21 21:02:15 +0800

    save

--- boot/smtp.go
+++ boot/smtp.go
@@ -2,13 +2,11 @@ package boot
 
 import (
 	"GTMS/conf"
-	"fmt"
 	"github.com/go-gomail/gomail"
 )
 
 //发送邮件
 func SendEmail(receiverAddress string, receiverName string, subject string, body string) (err error) {
-	fmt.Println(body)
 	cfg := conf.GetSmtpConfig()
 	m := gomail.NewMessage()
 	m.SetAddressHeader("From", cfg.Address, cfg.UserName) //发件人
