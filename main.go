b24775116c7fabcbe61b0147dad67f3f5da73e75
nian.zhou <nian.zhou@office.feng.com>
2019-03-27 00:00:32 +0800

    处理跨域问题

--- main.go
+++ main.go
@@ -6,14 +6,24 @@ import (
 	_ "GTMS/routers"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/plugins/cors"
 	"runtime"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU()) //多核运行
 	conf.LoadingConf()
+	//添加日志
 	logs.SetLogger(logs.AdapterMultiFile, `{"filename":"./runtime/log/project.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
 	logs.Async()
+	//处理跨域问题
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
 	boot.ConnectMySQL()
 	boot.ConnectRedis()
 	beego.Run()
