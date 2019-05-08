9c86b7e4e79cd59b8f60d7b0b3c1fd9d6d54f472
nian.zhou <nian.zhou@office.feng.com>
2019-05-08 19:41:19 +0800

    save

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func main() {
 	logs.Async()
 	//处理跨域问题
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Authorization", "X-Access-Token", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-		AllowCredentials: true,
+		AllowAllOrigins:  true,                                                                                                                                 //设置为true则所有域名都可以访问本网站接口
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},                                                                                  //允许的请求类型
+		AllowHeaders:     []string{"Origin", "Authorization", "X-Access-Token", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"}, //允许的头部信息
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},                            //允许暴露的头信息
+		AllowCredentials: true,                                                                                                                                 //允许共享AuthTuffic证书
 	}))
 	boot.ConnectMySQL()
 	boot.ConnectRedis()
